6e198a39b3e6944e719730b10632730b30b20598
Johannes Wachter <johannes.wachter@massiveart.com>
2016-12-10 08:50:30 +0100

    added list api

--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -4,13 +4,30 @@ namespace AppBundle\Controller;
 
 use App\Model\Page\Command\CreatePage;
 use App\Model\Page\Command\UpdatePage;
+use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
+use Sulu\Component\Rest\ListBuilder\Doctrine\FieldDescriptor\DoctrineFieldDescriptor;
+use Sulu\Component\Rest\ListBuilder\ListRepresentation;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PageController extends FOSRestController implements ClassResourceInterface
 {
+    const ENTITY_NAME = 'AppBundle:Page';
+
+    /**
+     * Returns all fields that can be used by list.
+     *
+     * @Get("pages/fields")
+     *
+     * @return Response
+     */
+    public function getFieldsAction()
+    {
+        return $this->handleView($this->view(array_values($this->getFieldDescriptors())));
+    }
+
     /**
      * Returns a single page identified by id.
      *
@@ -56,5 +73,53 @@ class PageController extends FOSRestController implements ClassResourceInterface
         return $this->handleView(
             $this->view($this->get('app.repository.page')->find($command->getPageId()->toString()))
         );
+    }
+
+    /**
+     * Shows all pages.
+     *
+     * @param Request $request
+     *
+     * @Get("pages")
+     *
+     * @return Response
+     */
+    public function cgetAction(Request $request)
+    {
+        $restHelper = $this->get('sulu_core.doctrine_rest_helper');
+        $factory = $this->get('sulu_core.doctrine_list_builder_factory');
+
+        $listBuilder = $factory->create(self::ENTITY_NAME);
+        $restHelper->initializeListBuilder($listBuilder, $this->getFieldDescriptors());
+        $results = $listBuilder->execute();
+
+        $list = new ListRepresentation(
+            $results,
+            'pages',
+            'get_pages',
+            $request->query->all(),
+            $listBuilder->getCurrentPage(),
+            $listBuilder->getLimit(),
+            $listBuilder->count()
+        );
+
+        return $this->handleView($this->view($list));
+    }
+
+    /**
+     * Returns array of existing field-descriptors.
+     *
+     * @return array
+     */
+    private function getFieldDescriptors()
+    {
+        return [
+            'id' => new DoctrineFieldDescriptor(
+                'id', 'id', self::ENTITY_NAME, 'public.id', [], true
+            ),
+            'title' => new DoctrineFieldDescriptor(
+                'title', 'title', self::ENTITY_NAME, 'public.title'
+            ),
+        ];
     }
 }
